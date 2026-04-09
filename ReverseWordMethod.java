84628154636be09670a5629fe78db8852707c583
Rk_Barath <rkbarath5717@gmail.com>
2026-04-09 19:01:23 +0530

    Update ReverseWordMethod.java

--- ReverseWordMethod.java
+++ ReverseWordMethod.java
@@ -1,9 +1,9 @@
 import java.util.*;
-public class ReverseWordMethod {
+public class WordReverseMethod {
     public static void main(String[] args) {
         Scanner scn=new Scanner(System.in);
         String s=scn.nextLine();
-        String [] words=s.split("");
+        String [] words=s.split(" ");//give one space
         for(int i=words.length-1;i>=0;i--){
             System.out.print(words[i]+" ");
         }
