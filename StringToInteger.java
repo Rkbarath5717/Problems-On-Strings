ba1008fc8813f40128bbe87bcae7d0fc3514e8a7
Barathlingam K <rkbarath5717@gmail.com>
2026-04-19 20:57:37 +0530

    Update StringToInteger.java

--- StringToInteger.java
+++ StringToInteger.java
@@ -3,7 +3,7 @@ public class StringToInteger {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         String s = scn.nextLine();
-        int num = Integer.parseInt(s);
+        int num = Integer.parseInt(s); //parseInt => depent on postion on number (100000,1000,100,10,1)
         System.out.println("String to Number = "+num);
     }
 }
