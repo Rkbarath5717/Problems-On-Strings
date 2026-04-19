2e84b428c0110081791abe107a1694176fc2a6bb
Barathlingam K <rkbarath5717@gmail.com>
2026-04-19 20:56:30 +0530

    Add files via upload

---
+++ StringToInteger.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class StringToInteger {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String s = scn.nextLine();
+        int num = Integer.parseInt(s);
+        System.out.println("String to Number = "+num);
+    }
+}
